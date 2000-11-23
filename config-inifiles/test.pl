a07ca8f4c9740b3abe4dcf678a785936ce0d631c
grail <grail@f8d1a983-3869-4788-a6c8-d31e7687f17a>
2000-11-23 05:06:54 +0000

    First part of fixes for bug 123150 - nitpicking

--- config-inifiles/test.pl
+++ config-inifiles/test.pl
@@ -6,7 +6,7 @@
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..30\n"; }
+BEGIN { $| = 1; print "1..35\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Config::IniFiles;
 $loaded = 1;
