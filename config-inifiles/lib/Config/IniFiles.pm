7757b40fab3f401fde1f26afbc73e8f0b62d4908
Shlomi Fish <shlomif@shlomifish.org>
2018-04-21 12:09:27 +0300

    - prepare for the new release.

--- config-inifiles/lib/Config/IniFiles.pm
+++ config-inifiles/lib/Config/IniFiles.pm
@@ -4,7 +4,7 @@ require 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '2.96';
+our $VERSION = '2.97';
 use Carp;
 use Symbol 'gensym', 'qualify_to_ref';    # For the 'any data type' hack
 use Fcntl qw( SEEK_SET SEEK_CUR );
